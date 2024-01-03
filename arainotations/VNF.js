867bff389ef501c55ce0cee37e997d7f00c2bb5e
Convindix <95302216+Convindix@users.noreply.github.com>
2024-01-03 13:24:24 -0600

    Split into multiple files, rewrite code more like Veblenum

--- arainotations/VNF.js
+++ arainotations/VNF.js
@@ -1,100 +1,17 @@
 class CSymbol { //Constant symbol
-  constructor(func) {
-    this.func = func; //Func is the symbol, like a 0-ary function symbol. Currently only "0" used
+  constructor(symbol) {
+    this.symbol = symbol; //Currently only "0" used
   }
 }
 
-class MultLambdaExp{ //A single "a*Lambda^b", a intended to be <Lambda
-  //Not sure how to make it so that CSymbol not a sybtype of this, but this is a subtype of Term
-  constructor(coeff, exp){
-    /*typeof coeff is LCNFTerm
-    typeof exp is LCNFTerm*/
-    this.coeff = coeff;
-    this.exp = exp;
-  }
-  static equ(a, b){
-    return JSON.stringify(a) == JSON.stringify(b);
-  }
-  static lss(a, b){
-    return LCNFTerm.lss(a.exp, b.exp) || (LCNFTerm.equ(a.exp, b.exp) && LCNFTerm.lss(a.coeff, b.coeff));
-  }
-}
-
-class LCNFTerm extends CSymbol { //Base-Lambda CNF, an array of MultLambdaExp's.
-  /*Note that implementation of "0" is rolled into MultLambdaExp (i.e. to use 0 in a hereditary LCNF representation, e.g. as in 3*L^(0*L^0)), use a MultLambdaExp as in 0*^L^0 or similar.
-    Assumptions:
-    Bases of any MultLambdaExp's appearing in this.arg match
-    All summands are >0*/
-  constructor(func, arg) {
-    /*typeof func is string
-    typeof arg is either undefined or an array of MultLambdaExps*/
-    super(func);
+class Phi{
+  /*
+  Phi with index=a and arg=b is phi(a,b)
+  typeof index is Term
+  typeof arg is Term*/
+  constructor(index, args){
+    this.index = index;
     this.arg = arg;
-  }
-  static equ(a, b) { //TODO: Change this to "practical equality"
-    return JSON.stringify(a) == JSON.stringify(b);
-  }
-  static lss(a, b) {
-    //Lexicographical comparison
-    for(var i = 0; i < a.args.length; i++){
-      if(i > b.args.length){
-        return false;
-      }
-      if(MultLambdaExp.lss(a.args[i], b.args[i])){
-        return true;
-      }else{
-        return false;
-      }
-    }
-    return false; //Equal
-  }
-  isStd() {
-    switch (this.func) {
-      case "0":
-        return true;
-      case "sum":
-        if (this.arg.length < 2) { //Sum with fewer than two summands
-          return false;
-        }
-        for (var i = 0; i < this.arg.length; i++) { //Summands in non-strictly descending order, all summands are wexps
-          if (this.arg[i].func != "wexp") {
-            return false;
-          }
-          if (i > 0 && LCNFTerm.lss(this.arg[i - 1], this.arg[i])) {
-            return false;
-          }
-        }
-        return true;
-      case "lexp":
-        return this.arg.isStd();
-    }
-  }
-  static parseToTerm(str) {
-    /*Parses string to CNFTerm, looking for these tokens:
-    0 - becomes CSymbol {func: '0'}
-    L^(a) - becomes Term {func: "lexp", arg: a}
-    a+b+...+z (expanded greedily) - becomes CSymbol {type: "sum", arg: [a,b,...,z]}
-    Will throw error for any string with unbalanced parentheses, since only the outermost pair of parentheses is removed at each step of recursion
-    */
-    if (str == "0") {
-      return new CSymbol('0');
-    } else if (hasCharOnBaseLevel(str, '+')) { //Sum
-      var summands = splitOnBaseChars(str, '+');
-      return new LCNFTerm("sum", summands.map(LCNFTerm.parseToTerm));
-    } else if (str.slice(0, 3) == "L^(" && str[str.length - 1] == ")") { //L^a, TODO: make this include the coefficients
-      return new LCNFTerm("lexp", LCNFTerm.parseToTerm(str.slice(3, -1)));
-    } else {
-      throw "Invalid term";
-    }
-  }
-}
-
-class VNFTerm extends CSymbol{
-  /* func is "0", "sum", or "phi"
-  args is array of Terms */
-  constructor(func, args){
-    super(func);
-    this.args = args;
   }
   static equ(a, b){ //TODO: Change this to "practical equality" (e.g. f(0,f(1,0)) == f(1,0))
     if(JSON.stringify(a) == JSON.stringify(b)){
@@ -193,6 +110,91 @@ class VNFTerm extends CSymbol{
       var argument = str.slice(2, -1);
       var args = splitOnBaseChars(argument, ',');
       return new VNFTerm("phi", args.map(VNFTerm.parseToTerm));
+    } else {
+      throw "Invalid term";
+    }
+  }
+}
+
+class MultLambdaExp{ //A single "a*Lambda^b", a intended to be <Lambda
+  //Not sure how to make it so that CSymbol not a sybtype of this, but this is a subtype of Term
+  constructor(coeff, exp){
+    /*typeof coeff is LCNFTerm
+    typeof exp is LCNFTerm*/
+    this.coeff = coeff;
+    this.exp = exp;
+  }
+  static equ(a, b){
+    return JSON.stringify(a) == JSON.stringify(b);
+  }
+  static lss(a, b){
+    return LCNFTerm.lss(a.exp, b.exp) || (LCNFTerm.equ(a.exp, b.exp) && LCNFTerm.lss(a.coeff, b.coeff));
+  }
+}
+
+class LCNFTerm extends CSymbol { //Base-Lambda CNF, an array of MultLambdaExp's.
+  /*Note that implementation of "0" is rolled into MultLambdaExp (i.e. to use 0 in a hereditary LCNF representation, e.g. as in 3*L^(0*L^0)), use a MultLambdaExp as in 0*^L^0 or similar.
+    Assumptions:
+    Bases of any MultLambdaExp's appearing in this.arg match
+    All summands are >0*/
+  constructor(func, arg) {
+    /*typeof func is string
+    typeof arg is either undefined or an array of MultLambdaExps*/
+    super(func);
+    this.arg = arg;
+  }
+  static equ(a, b) { //TODO: Change this to "practical equality"
+    return JSON.stringify(a) == JSON.stringify(b);
+  }
+  static lss(a, b) {
+    //Lexicographical comparison
+    for(var i = 0; i < a.args.length; i++){
+      if(i > b.args.length){
+        return false;
+      }
+      if(MultLambdaExp.lss(a.args[i], b.args[i])){
+        return true;
+      }else{
+        return false;
+      }
+    }
+    return false; //Equal
+  }
+  isStd() {
+    switch (this.func) {
+      case "0":
+        return true;
+      case "sum":
+        if (this.arg.length < 2) { //Sum with fewer than two summands
+          return false;
+        }
+        for (var i = 0; i < this.arg.length; i++) { //Summands in non-strictly descending order, all summands are wexps
+          if (this.arg[i].func != "wexp") {
+            return false;
+          }
+          if (i > 0 && LCNFTerm.lss(this.arg[i - 1], this.arg[i])) {
+            return false;
+          }
+        }
+        return true;
+      case "lexp":
+        return this.arg.isStd();
+    }
+  }
+  static parseToTerm(str) {
+    /*Parses string to CNFTerm, looking for these tokens:
+    0 - becomes CSymbol {func: '0'}
+    L^(a) - becomes Term {func: "lexp", arg: a}
+    a+b+...+z (expanded greedily) - becomes CSymbol {type: "sum", arg: [a,b,...,z]}
+    Will throw error for any string with unbalanced parentheses, since only the outermost pair of parentheses is removed at each step of recursion
+    */
+    if (str == "0") {
+      return new CSymbol('0');
+    } else if (hasCharOnBaseLevel(str, '+')) { //Sum
+      var summands = splitOnBaseChars(str, '+');
+      return new LCNFTerm("sum", summands.map(LCNFTerm.parseToTerm));
+    } else if (str.slice(0, 3) == "L^(" && str[str.length - 1] == ")") { //L^a, TODO: make this include the coefficients
+      return new LCNFTerm("lexp", LCNFTerm.parseToTerm(str.slice(3, -1)));
     } else {
       throw "Invalid term";
     }
